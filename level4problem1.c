e3cb6b23fb39460c74869eec3e771244aa6726c8
unknown <bala.devikhatuty@gmal.com>
2025-12-25 19:30:46 +0530

    update c_programming

---
+++ level4problem1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1;
+
+    while (i <= 5) {
+        if (i <= 5) {
+            printf("%d\n", i);
+        } else 
+        { }
+        i++;
+    }
+
+    return 0;
+}
