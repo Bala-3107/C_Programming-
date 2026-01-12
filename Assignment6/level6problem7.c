efb07a1506689ca3ab04dec21750f2d6309dfc1c
unknown <bala.devikhatuty@gmal.com>
2026-01-12 21:23:01 +0530

    update c_programming

---
+++ Assignment6/level6problem7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=11;
+    loop:
+    while(a<100)
+    {
+        if(((a%10)+(a/10))==7 &&  (a%2)==1)
+        {
+             printf("%d\n",a);
+        }   
+        a++;
+        goto loop;
+    }
+    return 0;
+}
